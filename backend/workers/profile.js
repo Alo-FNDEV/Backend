eb85f95ee7565d67b0c3b9d4166ab25f3e9b9210
zinx <63958372+zinx-YT@users.noreply.github.com>
2022-09-18 19:43:45 +0100

    Add files via upload

--- backend/workers/profile.js
+++ backend/workers/profile.js
@@ -3,8 +3,9 @@ const app = express.Router();
 
 app.all("/fortnite/api/game/v2/profile/:accountId/*/:command", (req, res) => {
     const profileID = req.query.profileId;
+    const rvn = req.params.command;
     var retJSON = {
-        profileRevision: rvn + 1,
+        profileRevision: rvn + 1 || 1,
         profileId: profileID,
         profileChangesBaseRevision: 1,
         profileChanges: [],
